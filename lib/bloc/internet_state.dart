073942b7610bf97456d0739c6d68139dabf7f1a2
amal <amalzhir2@gmail.com>
2023-12-07 17:17:44 +0100

    check connection Internet in device by Bloc architecture

---
+++ lib/bloc/internet_state.dart
@@ -0,0 +1,17 @@
+part of 'internet_bloc.dart';
+
+@immutable
+abstract class InternetState {}
+
+class InternetInitial extends InternetState {}
+
+class ConnectedState extends InternetState{
+  final String message;
+
+  ConnectedState({required this.message});
+}
+class NotConnectedInternet extends InternetState{
+  final String message;
+
+  NotConnectedInternet({required this.message});
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:connectivity_bloc_cubit/bloc/internet_bloc.dart';
+import 'package:connectivity_bloc_cubit/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => InternetBloc(),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+      ),
+    );
+  }
+}
